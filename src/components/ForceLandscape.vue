aa4ec3b5efd4bcd0c9c2f657b551fb38b4b43d89
Maxence <mottard.maxence@gmail.com>
2021-06-01 16:41:21 +0200

    force landscape on mobile (#3)
    * try to force landscape on mobile
    
    * remove useless branche from deploy.yml
    
    * remove useless comments
    
    * add padding to text

---
+++ src/components/ForceLandscape.vue
@@ -0,0 +1,46 @@
+<template>
+  <div v-if="modalIsOpen">
+    <p>Tournez votre écran pour passer en mode paysage.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ForceLandscape',
+  components: {},
+  data() {
+    return {
+      modalIsOpen: false
+    }
+  },
+  mounted () {
+    this.landscapeVerification()
+    window.addEventListener('resize', this.landscapeVerification);
+  },
+  methods: {
+    landscapeVerification() {
+      this.modalIsOpen = (window.innerHeight > window.innerWidth)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  div {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: black;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  p {
+    color: white;
+    font-size: 20px;
+    padding: 0 20px;
+  }
+</style>
