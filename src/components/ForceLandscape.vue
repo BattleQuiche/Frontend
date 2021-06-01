3f7c11b286cf68a0426ea3ffafa12968ed569eb6
maxencemottard <mottard.maxence@gmail.com>
2021-06-01 15:52:56 +0200

    try to force landscape on mobile

---
+++ src/components/ForceLandscape.vue
@@ -0,0 +1,47 @@
+<template>
+  <div v-if="modalIsOpen">
+    <p>Tournez votre écran pour passer en mode paysage.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ForceLandscape',
+  components: {},
+  data() {
+    return {
+      modalIsOpen: false
+    }
+  },
+  mounted () {
+    this.landscapeVerification()
+    window.addEventListener('resize', this.landscapeVerification);
+  },
+  methods: {
+    landscapeVerification() {
+      console.log('window.innerHeight', window.innerHeight)
+      console.log('window.innerWidth', window.innerWidth)
+      this.modalIsOpen = (window.innerHeight > window.innerWidth)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  div {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: black;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  p {
+    color: white;
+    font-size: 20px;
+  }
+</style>
