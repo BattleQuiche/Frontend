94a7b2026aa252fdbd1cd61c6261fc87ee98e382
Laroche-Nicolas <larochenicolas.info@gmail.com>
2021-06-02 17:18:34 +0200

    Init component inventory

---
+++ src/reusables/InventoryBarWidget.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="inventory">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InventoryBarWidget",
+  data() {
+    return {
+      inventory: {},
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
