94c0e29e8a2cbc405041faa05268007924297275
Maxence <mottard.maxence@gmail.com>
2021-06-02 16:42:17 +0200

    Feature/game card (#31)
    * generate json with data to test map
    
    * create all components for the grid
    
    * use assets
    
    * rename grid
    
    * use assets for watter
    
    * add debug mode with vuex
    
    * Revert "use assets for watter"
    
    This reverts commit c77bbd239abc2db56eb2f069f424266044aa7665.
    
    # Conflicts:
    #	src/components/Card/MapCase.vue
    
    * Revert "use assets"
    
    This reverts commit c9724a42819fd973c3afe1801a1d1b757152bb0b.
    
    # Conflicts:
    #	src/components/Party/CardGrid.vue
    #	src/components/Party/MapCase.vue
    #	src/components/Party/Party.vue
    
    * fix case size

--- src/components/Party/Party.vue
+++ src/components/Party/Party.vue
@@ -1,13 +1,53 @@
 <template>
-  <p></p>
+  <div class="party">
+    <card-grid :cases="mapObjects"/>
+
+    <button id="debug-button" @click="setDebugModeTo(!debug)">Debug Mode</button>
+  </div>
 </template>
 
 <script>
+import CardGrid from '../../components/Party/CardGrid'
+import testMap from '@/assets/map.test.json'
+import {mapActions, mapGetters} from 'vuex'
+
 export default {
-  name: 'Party'
+  name: 'Party',
+  components: {
+    CardGrid,
+  },
+  data() {
+    return {
+      mapObjects: testMap
+    }
+  },
+  methods: {
+    ...mapActions(['setDebugModeTo'])
+  },
+  computed: {
+    ...mapGetters(['debug']),
+  }
 }
 </script>
 
 <style scoped>
+  .party {
+    position: relative;
+    height: 100vh;
+    width: 100vw;
+    overflow: scroll;
+    scrollbar-width: none;
+  }
 
+  .party::-webkit-scrollbar {
+    display: none;
+  }
+
+  #debug-button {
+    position: fixed;
+    z-index: 998;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+  }
 </style>
---
+++ src/reusables/Grid.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="grid" :style="{ zIndex }">
+    <div class="grid__row" v-for="y in verticalCases" :key="y">
+      <span class="grid__case" v-for="x in horizontalCases" :key="x">
+        <slot v-bind="{ x, y }"/>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Grid',
+  data() {
+    return {
+      horizontalCases: [...Array(this.numberOfHorizontalCases).keys()],
+      verticalCases: [...Array(this.numberOfVerticalCases).keys()],
+    }
+  },
+  props: {
+    numberOfHorizontalCases: { type: Number, require: true },
+    numberOfVerticalCases: { type: Number, require: true },
+    zIndex: { type: Number, default: 1 },
+  },
+}
+</script>
+
+<style scoped>
+  .grid {
+    position: absolute;
+  }
+
+  .grid__row {
+    display: flex;
+  }
+
+  .grid__case {
+    display: block;
+    height: 60px;
+    width: 60px;
+  }
+</style>
