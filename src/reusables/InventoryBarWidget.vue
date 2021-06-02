cfcc3a8fdc34620733c9f1570f83e2b1fc79b329
Laroche-Nicolas <larochenicolas.info@gmail.com>
2021-06-02 18:20:00 +0200

    Init inventory bar with img clickable

--- src/reusables/InventoryBarWidget.vue
+++ src/reusables/InventoryBarWidget.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="inventory">
-    
+    <div v-for="item in inventory" :key="item.id" class="inventory--item">
+      <img :src="item.imageURL" alt="urlForImage" :title="item.title" @click="clickOnItem(item)">
+    </div>
   </div>
 </template>
 
@@ -9,7 +11,14 @@ export default {
   name: "InventoryBarWidget",
   data() {
     return {
-      inventory: {},
+      inventory: [{id:"1234", title: "Objet 1", imageURL: "https://via.placeholder.com/80"},
+        {id:"12323", title: "Objet 2", imageURL: "https://via.placeholder.com/80"},
+        {id:"123456cc2", title: "Objet 3", imageURL: "https://via.placeholder.com/80"}],
+    }
+  },
+  methods: {
+    clickOnItem(item) {
+      console.log(`l'id de l'objet est ${item.id}`)
     }
   }
 }
@@ -17,4 +26,15 @@ export default {
 
 <style scoped>
 
+.inventory {
+  background-color: lightgray;
+  display: flex;
+  border: 1px black solid;
+}
+
+.inventory--item {
+  min-height: 30px;
+  margin: 0 10px;
+  border: 1px red solid;
+}
 </style>
