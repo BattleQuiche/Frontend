283011c443fc08e20e20697f899a4c7877941d4a
Zakarya Tolba <zakarya.tolba@ynov.com>
2021-06-02 09:10:01 +0200

    Created temporary home and startgame views

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav id="Main-menu">
+        <ul class="menu">
+            <li>
+                <router-link :to="{ name: 'StartGame' }">
+                <!-- <NavLink :active="isActive" :href="href" @click="navigate">Hello</NavLink> -->
+                    Start a game
+                </router-link>
+            </li>
+            <!-- <li>
+                <router-link :to="{ name: 'startGame' }" class="Profile_link"
+                            data-title="Home">
+                    
+                            test
+                </router-link>
+            </li> -->
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "Home"
+};
+</script>
