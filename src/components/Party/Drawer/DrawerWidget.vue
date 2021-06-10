9f1c647d3ddc26d24c4ee0802eb929b2f5744281
Zakarya Tolba <zakarya.tolba@ynov.com>
2021-06-10 22:12:45 +0200

    Added open/close css + anim to the drawer widget

--- src/components/Party/Drawer/DrawerWidget.vue
+++ src/components/Party/Drawer/DrawerWidget.vue
@@ -18,3 +18,85 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+
+#drawersContainer {
+	width: 50%;
+	height: 50%;
+	position: absolute;
+	top: 20%;
+    z-index: 1000;
+	left: 0;
+	overflow: hidden; 
+    pointer-events: none;
+}
+
+.drawer {
+	width: 100%;
+	height: 100%;
+	position: absolute;
+	top: 0;
+    pointer-events: none;
+}
+.drawer * {
+  pointer-events: auto;
+}
+.drawer > input[type=checkbox] {
+	display: none;
+}
+.drawerPull {
+	display: inline-block;
+	width: 25px;
+	background: DarkGray;
+	text-align: center;
+	font-weight: bold;
+	font-size: 1.2em;
+	vertical-align: top;
+    padding: 70px 0;
+}
+.drawerContents {
+	position: absolute;
+	top: 0;
+	height: 100%;
+}
+.drawerPull {
+	position: absolute;
+	top: 50%;
+
+}
+.drawerPull:after {
+	content: "";
+}
+
+#leftDrawer {
+	left: -200px;
+}
+#leftDrawerContents {
+	left: 0;
+	width: 200px;
+	background-color: lightGray;
+    border-radius: 5%;
+	transition-property: left;
+	transition-duration: 0.5s;
+    transition-timing-function: linear;   
+}
+#leftDrawerPull {
+	left: 200px;
+	transition-property: left;
+	transition-duration: 0.5s;
+    transition-timing-function: linear;
+}
+#leftDrawerPull:after {
+	content: "\203a";
+}
+#leftDrawerCheck:checked + #leftDrawerContents {
+	left: 150px;
+}
+#leftDrawerCheck:checked + #leftDrawerContents + #leftDrawerPull {
+	left: 350px;
+}
+#leftDrawerCheck:checked + #leftDrawerContents + #leftDrawerPull:after {
+	content: "\2039";
+}
+</style>
