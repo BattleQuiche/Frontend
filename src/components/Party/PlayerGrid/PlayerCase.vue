ad77f8d7ce9984834cf29e53313c19cc9dbdc13d
maxencemottard <mottard.maxence@gmail.com>
2021-06-11 02:06:43 +0200

    optim

--- src/components/Party/PlayerGrid/PlayerCase.vue
+++ src/components/Party/PlayerGrid/PlayerCase.vue
@@ -1,10 +1,8 @@
 <template>
-  <div class="grid__case_content">
-    <img v-if="caseData === 'player_icon_1'" src="@/assets/players/player_icon_1.png" alt="">
-    <img v-else-if="caseData === 'player_icon_2'" src="@/assets/players/player_icon_2.png" alt="">
-    <img v-else-if="caseData === 'player_icon_3'" src="@/assets/players/player_icon_3.png" alt="">
-    <img v-else-if="caseData === 'player_icon_4'" src="@/assets/players/player_icon_4.png" alt="">
-  </div>
+  <img class="grid__case_content" v-if="caseData === 'player_icon_1'" src="@/assets/players/player_icon_1.png" alt="">
+  <img class="grid__case_content" v-else-if="caseData === 'player_icon_2'" src="@/assets/players/player_icon_2.png" alt="">
+  <img class="grid__case_content" v-else-if="caseData === 'player_icon_3'" src="@/assets/players/player_icon_3.png" alt="">
+  <img class="grid__case_content" v-else-if="caseData === 'player_icon_4'" src="@/assets/players/player_icon_4.png" alt="">
 </template>
 
 <script>
