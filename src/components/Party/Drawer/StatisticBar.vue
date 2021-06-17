26de50b58fe27526c71e7c83ab900923fceb758e
Zakarya Tolba <zakarya.tolba@ynov.com>
2021-06-17 23:14:44 +0200

    Created StatisticBar widget component

---
+++ src/components/Party/Drawer/StatisticBar.vue
@@ -0,0 +1,78 @@
+<template>
+ <div class="statisticBar">
+    <div class="statisticBarBg">
+      <div v-for="key in Array(5).keys()" :key="'item2_'+key" class="statisticElement">
+        <div class="heartEmpty" v-if="type == 'health'">&#9829;</div>
+        <div class="statDotEmpty" v-else/>
+      </div>
+    </div>
+    <div class="statisticBarData">
+      <div v-for="key in movementPoints" :key="'item3_'+key" class="statisticElement">
+        <div class="heart" v-if="type == 'health'">&#9829;</div>
+        <div class="statDot" v-else/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StatisticBar",
+  props: {
+    movementPoints: {
+      type: Number,
+      required: true,
+    },
+    type: { type: String },
+  },
+}
+</script>
+
+<style scoped>
+.statisticBar {
+    margin-top: 3px;
+    display: flex;
+    flex-direction: column;
+}
+
+.statisticBarData {
+    display: flex;
+    flex-direction: row;
+    position: fixed;
+}
+
+.statisticBarBg {
+    display: flex;
+    flex-direction: row;
+    position: relative;
+}
+
+.statDotEmpty {
+    width: 10px;
+    height: 10px;
+    background-color: darkgray;
+    border-radius: 100%;
+    margin: 1px;
+}
+
+.statDot {
+    width: 10px;
+    height: 10px;
+    background-color: white;
+    border-radius: 100%;
+    margin: 1px;
+}
+
+.statisticElement {
+    margin: 1px;
+
+}
+
+.heart {
+  color: red;
+}
+
+.heartEmpty {
+  color: rgb(172, 172, 172);
+}
+</style>
