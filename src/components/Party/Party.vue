a6f757c9ce1dd04f5403b4ea62403609af79a3de
Zakarya Tolba <zakarya.tolba@ynov.com>
2021-06-21 19:50:29 +0200

    Cleaned the code

--- src/components/Party/Party.vue
+++ src/components/Party/Party.vue
@@ -68,10 +68,7 @@ export default {
     async movePlayer(player, position) {
       try {
         const URL = 'https://wars.quiches.ovh/api/party/action'
-        // const players = this.players.map(pl => pl)
-        // const playerIndex = players.indexOf(player)
-        const result = await this.$http.put(URL, 
-        {
+        const result = await this.$http.put(URL, {
           "partyId": this.partyId,
           "userId": this.user._id,
           "actionType": "MOVE",
@@ -80,17 +77,10 @@ export default {
           "fromY": 0,
           "toX": position.x,
           "toY": position.y
-        }
-        )
-        this.setUser(result.data)
+        })
        } catch (err) {
          console.log(err)
        }
-      // const players = this.players.map(pl => pl)
-      // const playerIndex = players.indexOf(player)
-      // players[playerIndex] = { ...player, x: position.x, y: position.y }
-
-      // this.$set(this.$data, 'players', players)
     },
     handleSelectPlayerItem(item) {
       this.selectedItem = item;
