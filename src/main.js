a441327dcd207aa2d6b0ed5f9a758b9fbdba3c42
Laroche-Nicolas <larochenicolas.info@gmail.com>
2021-06-22 09:50:13 +0200

    Add .env config and variable

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import router from "./router";
 import './registerServiceWorker'
 import store from './store';
 import axios from 'axios'
+import dotenv from 'dotenv'
 
 Vue.prototype.$http = axios
+Vue.prototype.$env = process.env
 
+dotenv.config();
 
 Vue.config.productionTip = false
 
