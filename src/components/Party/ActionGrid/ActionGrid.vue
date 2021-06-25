bc97214bd77942aa8c068ead8afd558ee4453a17
maxencemottard <mottard.maxence@gmail.com>
2021-06-25 18:23:04 +0200

    use new icons

--- src/components/Party/ActionGrid/ActionGrid.vue
+++ src/components/Party/ActionGrid/ActionGrid.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="grid action-grid" :style="style">
-      <span class="grid__case" v-for="player in players" :key="player._id" :style="{ gridColumnStart: player.x + 1, gridRowStart: player.y + 1 }">
-        <img class="grid__case_content" :src="`/players/${player.icon}.png`" :alt="`${player.icon}.png`">
+      <span class="grid__case player-icon" v-for="player in players" :key="player._id" :style="{ gridColumnStart: player.x + 1, gridRowStart: player.y + 1 }">
+        <img class="grid__case_content " :src="`/players/${player.icon}.png`" :alt="`${player.icon}.png`">
       </span>
 
     <span v-for="aroundCase in movableCases" :key="JSON.stringify(aroundCase)"
@@ -131,6 +131,20 @@ export default {
 .grid__case_content img {
   max-width: 100%;
   max-height: 100%;
+}
+
+.player-icon {
+  position: relative;
+}
+
+.player-icon img {
+  max-width: none;
+  max-height: none;
+  height: 32px;
+  width: 16px;
+  position: absolute;
+  top: -100%;
+  bottom: 0;
 }
 </style>
 
