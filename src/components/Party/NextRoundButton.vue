cea65a187f71837639877325648eafc9bd0f3cd8
Nicolas Laroche <44775996+Laroche-Nicolas@users.noreply.github.com>
2021-06-25 17:52:26 +0200

    Init component for endRound of a player (#56)

---
+++ src/components/Party/NextRoundButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <button @click="endRound" class="buttonEndTurn" >Terminer le tour.</button>
+</template>
+
+<script>
+import {mapGetters} from "vuex";
+
+export default {
+  name: "NextRoundButton",
+  methods: {
+    async endRound () {
+      try {
+        await this.$http.post(`${this.$env.VUE_APP_API_BASE_URL}/party/next-round`, {
+          partyId: this.party.partyId,
+          userId: this.user._id
+        })
+
+        console.log("gfhjdik")
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  },
+  computed: {
+    ...mapGetters(['party', 'user']),
+  }
+}
+</script>
+
+<style scoped>
+.buttonEndTurn {
+  position: fixed;
+  bottom: 0;
+  left: 85%;
+  transform: translateX(-50%);
+  z-index: 50;
+  background-color: lightgray;
+  padding: 18px;
+}
+</style>
